74ece60b2a9478cca3556d7b747ef29ee72e9806
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-08-06 14:53:27 +0530

    fix delete function

--- PingPost/tweet/views.py
+++ PingPost/tweet/views.py
@@ -30,7 +30,7 @@ def tweet_create(request):
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == 'POST':
-        TweetForms(request.POST, request.FILES, instance=tweet)
+        form = TweetForms(request.POST, request.FILES, instance=tweet)
         if form.is_valid():
             tweet = form.save(commit=False)
             tweet.user = request.user
